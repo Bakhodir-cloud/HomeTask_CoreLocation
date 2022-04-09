706eadd5c7366d82dc502607330c2cc399c9ccb9
Bakhodir <btoshniyozov@gmail.com>
2022-04-09 13:51:31 +0500

    Initial Commit

---
+++ HomeTask_CoreLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeTask_CoreLocation
+//
+//  Created by baxa on 09/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
